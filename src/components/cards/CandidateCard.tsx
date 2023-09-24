d555234ee3dcb4bc5c50d3eb234b176132d63f38
harshitratan <harshitratanshukla@gmail.com>
2023-09-24 17:36:02 +0530

    checkbox is added

--- src/components/cards/CandidateCard.tsx
+++ src/components/cards/CandidateCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, Card } from "antd";
+import { Avatar, Card, Checkbox } from "antd";
 import { Typography } from "antd";
 import Meta from "antd/es/card/Meta";
 const { Title } = Typography;
@@ -15,115 +15,118 @@ const CandidateCard = () => {
           marginTop: 16,
         }}
       >
-        <Meta
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          avatar={
-            <Avatar
-              style={{
-                backgroundColor: "#D0E1FF",
-                color: "rgb(70 112 187)",
-                fontSize: "1rem",
-                height: "3.5rem",
-                width: "3.5rem",
-                display: "flex",
-                textAlign: "center",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              AS
-            </Avatar>
-          }
-          title={
-            <div>
-              <Title
+        <Checkbox>
+          <Meta
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginLeft: "1.5rem",
+            }}
+            avatar={
+              <Avatar
                 style={{
-                  color: "#000",
-                  fontFamily: "Poppins",
-                  fontSize: "16px",
-                  fontStyle: "normal",
-                  fontWeight: "600",
-                  lineHeight: "normal",
+                  backgroundColor: "#D0E1FF",
+                  color: "rgb(70 112 187)",
+                  fontSize: "1rem",
+                  height: "3.5rem",
+                  width: "3.5rem",
+                  display: "flex",
+                  textAlign: "center",
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
               >
-                Aaliyah Sanderson
-              </Title>
-              <Title
-                style={{
-                  marginTop: "0.5rem",
-                  color: "#000",
-                  fontFamily: "Poppins",
-                  fontSize: "14px",
-                  fontStyle: "normal",
-                  fontWeight: "500",
-                  lineHeight: "normal",
-                }}
-              >
-                Riyadh, Saudi Arabia
-              </Title>
-              <Title
-                style={{
-                  marginTop: "0.5rem",
-                  color: "#000",
-                  fontFamily: "Poppins",
-                  fontSize: "14px",
-                  fontStyle: "normal",
-                  fontWeight: "300",
-                  lineHeight: "normal",
-                }}
-              >
-                Bachelor - Cambridge University (2023 - 2023)
-              </Title>
-              {hastagArray.length > 0 &&
-                hastagArray.map((hastag, index) => (
-                  <Title
-                    key={index}
-                    style={{
-                      marginTop: "0.25rem",
-                      color: "#1D4ED8",
-                      fontFamily: "Poppins",
-                      fontSize: "12px",
-                      fontStyle: "normal",
-                      fontWeight: "400",
-                      lineHeight: "normal",
-                      float: "left",
-                      marginRight: 10,
-                    }}
-                  >
-                    {hastag}
-                  </Title>
-                ))}
-              <br />
-              {chipsArray.length > 0 &&
-                chipsArray.map((value, index) => (
-                  <Title
-                    key={index}
-                    style={{
-                      marginTop: "0.25rem",
-                      width: "80px",
-                      fontSize: "12px",
-                      display: "flex",
-                      padding: "2px 10px",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      gap: "10px",
-                      borderRadius: "4px",
-                      background: "#F3FAFC",
-                      color: "#037092",
-                      float: "left",
-                      marginRight: 10,
-                    }}
-                  >
-                    {value}
-                  </Title>
-                ))}
-            </div>
-          }
-        />
+                AS
+              </Avatar>
+            }
+            title={
+              <div>
+                <Title
+                  style={{
+                    color: "#000",
+                    fontFamily: "Poppins",
+                    fontSize: "16px",
+                    fontStyle: "normal",
+                    fontWeight: "600",
+                    lineHeight: "normal",
+                  }}
+                >
+                  Aaliyah Sanderson
+                </Title>
+                <Title
+                  style={{
+                    marginTop: "0.5rem",
+                    color: "#000",
+                    fontFamily: "Poppins",
+                    fontSize: "14px",
+                    fontStyle: "normal",
+                    fontWeight: "500",
+                    lineHeight: "normal",
+                  }}
+                >
+                  Riyadh, Saudi Arabia
+                </Title>
+                <Title
+                  style={{
+                    marginTop: "0.5rem",
+                    color: "#000",
+                    fontFamily: "Poppins",
+                    fontSize: "14px",
+                    fontStyle: "normal",
+                    fontWeight: "300",
+                    lineHeight: "normal",
+                  }}
+                >
+                  Bachelor - Cambridge University (2023 - 2023)
+                </Title>
+                {hastagArray.length > 0 &&
+                  hastagArray.map((hastag, index) => (
+                    <Title
+                      key={index}
+                      style={{
+                        marginTop: "0.25rem",
+                        color: "#1D4ED8",
+                        fontFamily: "Poppins",
+                        fontSize: "12px",
+                        fontStyle: "normal",
+                        fontWeight: "400",
+                        lineHeight: "normal",
+                        float: "left",
+                        marginRight: 10,
+                      }}
+                    >
+                      {hastag}
+                    </Title>
+                  ))}
+                <br />
+                {chipsArray.length > 0 &&
+                  chipsArray.map((value, index) => (
+                    <Title
+                      key={index}
+                      style={{
+                        marginTop: "0.25rem",
+                        width: "80px",
+                        fontSize: "12px",
+                        display: "flex",
+                        padding: "2px 10px",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        gap: "10px",
+                        borderRadius: "4px",
+                        background: "#F3FAFC",
+                        color: "#037092",
+                        float: "left",
+                        marginRight: 10,
+                      }}
+                    >
+                      {value}
+                    </Title>
+                  ))}
+              </div>
+            }
+          />
+        </Checkbox>
       </Card>
     </>
   );
